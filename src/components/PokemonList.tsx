237b4878fe0f944e24defdca7c696cd2df6548e8
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2022-04-14 23:49:02 -0500

    refactor buttons next previous

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -2,30 +2,18 @@ import { usePokemon } from "../hooks/usePokemon";
 import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
 import { Loading } from "./Loading";
 import { PokeCard } from "./pokeCard";
-import { useState } from 'react';
+import { FC, useState } from 'react';
 
-export const PokemonList = () => {
-  const { isLoading, pokemons } = usePokemon();
-  const [currentPage, setcurrentPage] = useState(0);
-  const filterPokemons = (): Pokemon[] => {
-    return pokemons.slice(currentPage, currentPage + 5);
-  };
+interface Props {
+  pokemonList?: Pokemon[];
+}
 
-  const nextPage = () => {
-    setcurrentPage( currentPage + 5 );
-  }
-
-  const previousPage = () => {
-    if(currentPage > 0 )
-    setcurrentPage( currentPage - 5 );
-  }
+export const PokemonList: FC<Props> = ({ pokemonList }) => {
+  const { isLoading } = usePokemon();
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 ">
-      <button className="btn btn-primary  mb-5" onClick={ previousPage } >Anteriores</button>
-      &nbsp;
-      <button className="btn btn-primary mb-5" onClick={ nextPage }>siguientes</button>
-      {filterPokemons().map((poke) => (
+      {pokemonList!.map((poke) => (
         <PokeCard key={poke.id} {...poke} />
       ))}
       {isLoading && <Loading />}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import { PokemonList } from "../components/PokemonList";
+import { usePokemon } from "../hooks/usePokemon";
+import { Pokemon } from "../interfaces/fetchAllPokemonResponse";
 
 export const HomePage = () => {
+
+  const { pokemons } = usePokemon();
+  const [currentPage, setcurrentPage] = useState(0);
+  const filterPokemons = (): Pokemon[] => {
+    return pokemons.slice(currentPage, currentPage + 5);
+  };
+
+
+  const nextPage = () => {
+    setcurrentPage( currentPage + 5 );
+  }
+
+  const previousPage = () => {
+    if(currentPage > 0 )
+    setcurrentPage( currentPage - 5 );
+  }
+
   return (
     <>
       <h1> Pokemon List </h1>
       <hr />
-      <PokemonList />
+      <button className="btn btn-primary  mb-5" onClick={ previousPage } >Anteriores</button>
+      &nbsp;
+      <button className="btn btn-primary mb-5" onClick={ nextPage }>siguientes</button>
+      <PokemonList pokemonList={filterPokemons()} />
     </>
   );
 };
